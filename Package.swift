0fe9d1dae3a71a9f3fa7fa4d91e8b5cb069a8815
w9i <50361448+w9i@users.noreply.github.com>
2022-05-17 16:45:06 +0800

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Domain",
-            dependencies: []),
+            dependencies: ["Common"]),
         .testTarget(
             name: "DomainTests",
             dependencies: ["Domain"]),
